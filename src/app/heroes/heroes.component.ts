69ac12183d957d491b58e615d09553d42073dead
xunmao <37928881+xunmao@users.noreply.github.com>
2020-12-26 22:30:30 +0800

    Add services. (#4)
    * Add HeroService.
    
    * Add MessageComponent and MessageService.
    
    * Modify HeroService to utilize Observable object.
    
    * Refactor HeroService and MessageService.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,15 +9,27 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(
+      (heroes) => (this.heroes = heroes),
+      (error) => console.log(error)
+    );
+  }
 
   onSelect(hero: Hero): void {
-    console.log('selected a hero: ' + hero.name);
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: selected hero id=${hero.id}`);
   }
 }
